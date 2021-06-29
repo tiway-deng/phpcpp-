35a1062dcc3c68eb25b0fd81b09d28b90a1593e5
tiway <501620409@qq.com>
2021-06-29 17:07:42 +0800

    cpp 第一个拓展

---
+++ Tiway-1/main.cpp
@@ -0,0 +1,48 @@
+#include <phpcpp.h>
+#include <iostream>
+
+void example()
+{
+    // the C++ equivalent of the echo() function
+    Php::out << "example output" << std::endl;
+
+    // generate output without a newline, and ensure that it is flushed
+    Php::out << "example output" << std::flush;
+
+    // or call the flush() method
+    Php::out << "example output";
+    Php::out.flush();
+
+    // just like all PHP functions, you can call the echo() function 
+    // from C++ code as well
+    Php::echo("Example output\n");
+
+    Php::Value 
+    
+}  
+
+/**
+ *  tell the compiler that the get_module is a pure C function
+ */
+extern "C" {
+    
+    /**
+     *  Function that is called by PHP right after the PHP process
+     *  has started, and that returns an address of an internal PHP
+     *  strucure with all the details and features of your extension
+     *
+     *  @return void*   a pointer to an address that is understood by PHP
+     */
+    PHPCPP_EXPORT void *get_module() 
+    {
+        // static(!) Php::Extension object that should stay in memory
+        // for the entire duration of the process (that's why it's static)
+        static Php::Extension extension("tiway", "1.0");
+        
+        // @todo    add your own functions, classes, namespaces to the extension
+        extension.add<example>("example");
+        
+        // return the extension
+        return extension;
+    }
+}
